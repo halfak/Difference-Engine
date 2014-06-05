3660c5e2a4dde6dda3cf98c159a83044d840c736
halfak <aaron.halfaker@gmail.com>
2014-06-05 08:04:01 -0500

    Fixed LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def requirements(fname):
+	for line in open(os.path.join(os.path.dirname(__file__), fname)):
+		yield line.strip()
+
+setup(
+	name = "diffengine",
+	version = read('VERSION').strip(),
+	author = "Aaron Halfaker",
+	author_email = "ahalfaker@wikimedia.org",
+	description = ("A difference tracker for MediaWiki."),
+	license = "MIT",
+	url = "https://github.com/halfak/Difference-Engine",
+	py_modules = ['diffengine'],
+	long_description = read('README.rst'),
+	install_requires = [],
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Monitoring"
+	],
+)
