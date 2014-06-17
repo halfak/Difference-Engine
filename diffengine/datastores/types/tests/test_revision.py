69b2fa0b971ed2e6ea5edffb1609a0cafca25336
halfak <aaron.halfaker@gmail.com>
2014-06-16 21:56:42 -0500

    One more test.

---
+++ diffengine/datastores/types/tests/test_revision.py
@@ -0,0 +1,23 @@
+from nose.tools import eq_
+
+from ..delta import Delta
+from ..operations import Insert, Persist, Remove
+from ..user import User
+from ..revision import Revision
+
+def test_revision():
+    rev_id = 10
+    timestamp = 1234567890
+    page_id = 12
+    user = User(10, "foobar")
+    delta = Delta("foobar_matcher", 55, 35, [Insert(0, 2, ["who", "?"])])
+    
+    revision = Revision(rev_id, timestamp, page_id, user, delta)
+    
+    r2 = Revision(revision.to_json())
+    eq_(revision, r2)
+    eq_(r2.rev_id, rev_id)
+    eq_(r2.timestamp, timestamp)
+    eq_(r2.page_id, page_id)
+    eq_(r2.user, user)
+    eq_(r2.delta, delta)
